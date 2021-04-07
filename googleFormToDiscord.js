0ed48714aff8eedb8db13ac54433893a49e852e3
jdfksfhdsfj <82116692+jdfksfhdsfj@users.noreply.github.com>
2021-04-07 14:46:46 -0400

    Update googleFormToDiscord.js

--- googleFormToDiscord.js
+++ googleFormToDiscord.js
@@ -1,10 +1,10 @@
 /* global UrlFetchApp */
 'use strict';
 /* DO NOT EDIT ABOVE THIS LINE */
-const webhookURL = ''; // Paste your webhook URL within the simple quotes.
-const title = ''; // Specify the title of the embed within the simple quotes. If not provided, it will default to the form's title.
-const color = 7506394; // Color of the embed in decimal. You can use this hexadecimal to decimal converter: https://www.binaryhexconverter.com/hex-to-decimal-converter
-const timeZone = 'America/Chicago'; // Replace with your timezone code if necessary. Get it from here: https://gist.github.com/diogocapela/12c6617fc87607d11fd62d2a4f42b02a
+const webhookURL = 'https://discord.com/api/webhooks/829346032898670594/ov4sB0aXpGLhJnQukYqA3szJLa5gHiDvYDrs5z2dtHXXOKGywr7z0-0KMUYiOyWfG-rS'; // Paste your webhook URL within the simple quotes.
+const title = 'new application'; // Specify the title of the embed within the simple quotes. If not provided, it will default to the form's title.
+const color = #946240; // Color of the embed in decimal. You can use this hexadecimal to decimal converter: https://www.binaryhexconverter.com/hex-to-decimal-converter
+const timeZone = 'America'; // Replace with your timezone code if necessary. Get it from here: https://gist.github.com/diogocapela/12c6617fc87607d11fd62d2a4f42b02a
 /* DO NOT EDIT UNDER THIS LINE */
 
 const onFormSubmit = (form) => {
